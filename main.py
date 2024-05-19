d2a3ecac1a23fe6efb90181d275d033d911f4a7f
Felipe Marcelino <felipe99marcelino@hotmail.com>
2024-05-19 14:04:15 -0300

    feat: integraçao bd alunos

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ import mysql.connector
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'PJI1102023;'
 
-# conexao = mysql.connector.connect(
-#     host="localhost",       # Endereço do servidor MySQL
-#     user="root",     # Nome de usuário do MySQL
-#     password="",   # Senha do MySQL
-#     database="projeto_escola"  # Nome do banco de dados
-# )
+conexao = mysql.connector.connect(
+    host="localhost",       # Endereço do servidor MySQL
+    user="root",     # Nome de usuário do MySQL
+    password="",   # Senha do MySQL
+    database="projeto_escola"  # Nome do banco de dados
+)
 
-# if conexao.is_connected():
-#     print("Conexão bem-sucedida!")
+if conexao.is_connected():
+    print("Conexão bem-sucedida!")
 
-# cursor = conexao.cursor()
+cursor = conexao.cursor()
 
 
 @app.route('/')
 def home(): 
-    # cursor.execute("SELECT * FROM alunos")
-    # data = cursor.fetchall()
-    return render_template('index.html')
+    cursor.execute("SELECT * FROM mensagem")
+    data = cursor.fetchall()
+    return render_template('index.html', avisos=data)
 
 @app.route('/consultar')
 def consultar(): 
@@ -47,6 +47,48 @@ def infos():
         return redirect('/painel')
     else:
         return redirect('/professor')
+
+@app.route('/boletim', methods=['POST'])
+def boletim():
+    cpf_ra = request.form.get('cpf_ra')
+
+    if not cpf_ra:
+        return redirect('/consultar')
+
+    cursor = conexao.cursor(dictionary=True)
+
+    # Primeira query para buscar os alunos
+    query = """
+    SELECT alunos.id, alunos.nome, alunos.ra, alunos.cpf_responsavel 
+    FROM alunos 
+    WHERE alunos.ra = %s OR alunos.cpf_responsavel = %s
+    """
+    cursor.execute(query, (cpf_ra, cpf_ra))
+    alunos = cursor.fetchall()
+
+    if not alunos:
+        cursor.close()
+        return render_template('aluno.html', erro='Nenhum resultado encontrado')
+
+    # Segunda query para buscar as notas para cada aluno
+    resultados = []
+    for aluno in alunos:
+        id = aluno['id']
+
+        query_notas = """
+        SELECT alunos_materias.nota, alunos_materias.bimestre, alunos_materias.observacao, materias.materia
+        FROM alunos_materias 
+        INNER JOIN materias ON alunos_materias.materias_id = materias.id 
+        WHERE alunos_materias.alunos_id = %s
+        """
+        cursor.execute(query_notas, (id,))
+        materias = cursor.fetchall()
+        print(materias)
+        aluno['materias'] = materias
+        resultados.append(aluno)
+
+    cursor.close()
+    return render_template('aluno.html', alunos=resultados)
 
 if __name__ in "__main__":
     app.run(debug=True)
